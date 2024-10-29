2ce741390da65e51585b47346caafe95663ea69d
unzeelaarif237 <unzeelaarifarif@gmail.com>
2024-10-29 18:11:57 +0500

    3pdf

---
+++ app.js
@@ -0,0 +1,27 @@
+// 1
+var age = 17;
+alert("My age is"+ " " + age);
+// 2
+let visitCount = 0; 
+
+        if (visitCount === 0) {
+            visitCount++; 
+            alert( "You have visited this site"+ " " +  visitCount +  " " +  "time.");
+        } else {
+            visitCount++; 
+            alert( "You have visited this site"+ " " +  visitCount +  " " +  "time.");
+        }
+
+        // 3
+
+        var birthYear = 2007;
+        alert("My birth year is" + " " +  birthYear)
+
+        // 4
+
+        var askName = prompt("Whats your full name?");
+        var askProduct = prompt("whats your product?");
+        var askQuantity = prompt("Quantity of your product?");
+        document.write(askName + " " + "ordered " + askQuantity +" " + askProduct + "'s" + " "+  "on Huma clothing store." );
+
+        
